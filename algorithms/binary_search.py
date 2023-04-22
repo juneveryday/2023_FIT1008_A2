b8e4779f6970856ed3d6c215b13981bdf7c69850
AdittiGupta <agup0031@student.monash.edu>
2023-04-22 21:42:34 +0800

    Task 5 completed

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -3,7 +3,7 @@ from typing import TypeVar
 
 T = TypeVar("T")
 
-def binary_search(l: list[T], item: T) -> int:
+def binary_search(l: list[T], item: T, key = lambda x:x) -> int:
     """
     Utilise the binary search algorithm to find the index where a particular element would be stored.
 
@@ -15,23 +15,24 @@ def binary_search(l: list[T], item: T) -> int:
     Best Case Complexity: O(1), when middle index contains item.
     Worst Case Complexity: O(log(N)), where N is the length of l.
     """
-    return _binary_search_aux(l, item, 0, len(l))
+    return _binary_search_aux(l, item, 0, len(l), key)
 
-def _binary_search_aux(l: list[T], item: T, lo: int, hi: int) -> int:
+def _binary_search_aux(l: list[T], item: T, lo: int, hi: int , key = lambda x:x) -> int:
     """
     Auxilliary method used by binary search.
     lo: smallest index where the return value could be.
     hi: largest index where the return value could be.
     """
     if lo == hi:
-        return lo
+        #return lo
+        raise KeyError("Item" ,item, "does not exist")
     mid = (hi + lo) // 2
-    if l[mid] > item:
+    if key(l[mid]) > key(item):
         # Item would be before mid
-        return _binary_search_aux(l, item, lo, mid)
-    elif l[mid] < item:
+        return _binary_search_aux(l, item, lo, mid, key)
+    elif key(l[mid]) < key(item):
         # Item would be after mid
-        return _binary_search_aux(l, item, mid+1, hi)
-    elif l[mid] == item:
+        return _binary_search_aux(l, item, mid+1, hi, key)
+    elif key(l[mid]) == key(item):
         return mid
     raise ValueError(f"Comparison operator poorly implemented {item} and {l[mid]} cannot be compared.")
--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,14 +1,23 @@
 from __future__ import annotations
 
 from mountain import Mountain
+from data_structures.hash_table import LinearProbeTable
+from algorithms.mergesort import mergesort
+from algorithms.binary_search import binary_search
 
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+
+        self.sorted_mountain_list = []
+
 
     def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
+        rank = binary_search (l = self.sorted_mountain_list, item = mountain, key = lambda a : (a.length , a.name))
+        return rank
+
 
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+        self.sorted_mountain_list = mergesort(l = self.sorted_mountain_list + mountains, key = lambda a : (a.length , a.name))
+ 
+        
